5fe2a5b1689e3f4c616d1edf51552eef0c075318
deom1203 <Deom1203@mac-mini-de-david.local>
2017-03-01 10:37:47 -0300

    Initial Commit

---
+++ Pizzas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pizzas
+//
+//  Created by David Osses Mena on 01-03-17.
+//  Copyright © 2017 David Osses Mena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
